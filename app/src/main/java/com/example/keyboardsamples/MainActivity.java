70a4e2d016421df184c127556829484b94e51a24
Salvi <salvi13@MacBook-Air-de-Salvi.local>
2019-12-05 17:56:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/keyboardsamples/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.keyboardsamples;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    public void print(View V){
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        TextView phonenumber = (TextView) findViewById(R.id.editText);
+        String str=spinner.getSelectedItem().toString();
+        str=str+"-> "+phonenumber.getText().toString();
+        TextView phonelist = (TextView) findViewById(R.id.textView);
+        phonelist.setText("Phone Number:  "+str);
+        showtoast(str);
+    }
+
+
+
+
+
+
+    public void showtoast(CharSequence text){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+}
+
